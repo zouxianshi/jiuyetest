4e81b9568a5e408d09dcb62a043458d34595ba19
unknown <1813561379@qq.com>
2019-10-30 18:37:28 +0800

    JACOCO

--- src/main/java/com/test/test.java
+++
@@ -1,9 +0,0 @@
-package com.test;
-import org.junit.Test;
-public class test {
-    @Test
-    public void main(String[] args){
-        Long a= 20922789888000L;
-        System.out.println(a.intValue());
-    }
-}
